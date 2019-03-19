55a5a68a7b8147744277b4eeed48c28620e3ebbb
Your Name <you@example.com>
2019-03-19 23:27:26 +0000

    fixed install script to copy plugins for correct dir

--- install.sh
+++ install.sh
@@ -3,3 +3,5 @@ mkdir /usr/local/lib/rip-lib
 cpanm -l /usr/local/lib/rip-lib Parse::Win32Registry
 cp rip /usr/local/bin
 chmod +x /usr/local/bin/rip
+mkdir /usr/local/bin/plugins
+cp -a plugins/. /usr/local/bin/plugins
